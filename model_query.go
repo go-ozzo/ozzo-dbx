0270400251f218b482ad1406f3ee7e9741c61835
Qiang Xue <qiang.xue@gmail.com>
2016-07-11 08:09:32 -0400

    ...

---
+++ model_query.go
@@ -0,0 +1,28 @@
+package dbx
+
+type TableModel interface {
+	TableName() string
+}
+
+type ModelQuery struct {
+	model        TableModel
+	fieldMapFunc FieldMapFunc
+}
+
+func (q *ModelQuery) Create(attrs ...string) error {
+	table := q.model.TableName()
+	/*
+		1. determine table name
+		2. determine field names and values
+		3. call Insert()
+	*/
+	return nil
+}
+
+func (q *ModelQuery) Update(attrs ...string) error {
+	return nil
+}
+
+func (q *ModelQuery) Delete() error {
+	return nil
+}
