a91a50be706567d2e6a12cb62d8863d9fa3dc6b6
Qiang Xue <qiang.xue@gmail.com>
2016-07-12 07:22:22 -0400

    ...

--- model_query.go
+++ model_query.go
@@ -1,12 +1,34 @@
 package dbx
 
+import "reflect"
+
 type TableModel interface {
 	TableName() string
 }
 
 type ModelQuery struct {
-	model        TableModel
+	tableName    string
+	model        interface{}
 	fieldMapFunc FieldMapFunc
+}
+
+func NewModelQuery(model interface{}, fmf FieldMapFunc) *ModelQuery {
+	mq := &ModelQuery{
+		model:        model,
+		fieldMapFunc: fmf,
+	}
+	tm, ok := model.(TableModel)
+	if ok {
+		mq.tableName = tm.TableName()
+	} else {
+		t := reflect.TypeOf(model)
+		if t.Kind() == reflect.Interface || t.Kind() == reflect.Ptr {
+			t = t.Elem()
+		}
+		// todo
+		mq.tableName = t.Name()
+	}
+	return mq
 }
 
 func (q *ModelQuery) Create(attrs ...string) error {
